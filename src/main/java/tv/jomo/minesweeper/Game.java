83e6576d904bfa52af466e910c7e13702d31f543
jomo <github@jomo.tv>
2015-04-30 17:11:36 +0200

    add menu

--- src/main/java/tv/jomo/minesweeper/Game.java
+++ src/main/java/tv/jomo/minesweeper/Game.java
@@ -1,7 +1,12 @@
 package tv.jomo.minesweeper;
 
+import javax.swing.Box;
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Game extends JFrame {
 
@@ -15,10 +20,44 @@ public class Game extends JFrame {
 
   private Game(String s) {
     super(s);
+    addMenu();
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     start();
     setResizable(false);
     setVisible(true);
+  }
+
+  private void addMenu() {
+    JMenuBar menuBar = new JMenuBar();
+
+    JMenuItem newGameMenu = new JMenuItem("New Game");
+    newGameMenu.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        start();
+      }
+    });
+    menuBar.add(newGameMenu);
+
+    JMenuItem gamePropertyMenu = new JMenuItem("Game Properties");
+    gamePropertyMenu.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(Game.this, "TODO", "Game Properties", JOptionPane.PLAIN_MESSAGE);
+      }
+    });
+    menuBar.add(gamePropertyMenu);
+
+    // Glue = Spacer (because Java)
+    menuBar.add(Box.createHorizontalGlue());
+
+    JMenuItem aboutMenu = new JMenuItem("About");
+    aboutMenu.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(Game.this, "Created by jomo in 2015\nSource Code: https://github.com/jomo/CAFEB0MB/issues/2", "About CAFEB0MB", JOptionPane.PLAIN_MESSAGE);
+      }
+    });
+    menuBar.add(aboutMenu);
+
+    setJMenuBar(menuBar);
   }
 
   private void start() {
